463d65e1da3048ef2081943d0b5cb22c8b19257d
nurialis <64111847+nurialis@users.noreply.github.com>
2020-05-08 14:35:19 -0500

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,7 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("New Class");
+        
+        // We can updated notes from GitHub...
     }
 }
