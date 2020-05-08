2de4b462249fab442f983ddb3e62b397de7cbf7d
nurialis <nurialis34@gmail.com>
2020-05-08 14:10:43 -0500

    add CreateChannel.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+        System.out.println("We love you Asya");
+        System.out.println("Creating channel function is working");
+    }
+}
